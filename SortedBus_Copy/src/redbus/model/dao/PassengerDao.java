fa53f2f54bb3a1edb71e1e7ef931ad02cfc0ae23
karanmidha <kmidha1@sapient.com>
2016-08-30 11:43:04 +0530

    fifth change Commented added in the Passengerdao in the function
    getMaxPid

--- SortedBus_Copy/src/redbus/model/dao/PassengerDao.java
+++ SortedBus_Copy/src/redbus/model/dao/PassengerDao.java
@@ -9,6 +9,7 @@ import redbus.model.pojo.Schedule;
 public interface PassengerDao {
 //The pId will be generated using a for loop at the time of sign up. The value of pId will be kept as static.
 	//Another comment added
+	//2nd comment added
 	public Passenger insertPassenger(Passenger p);// Includes implicit calling to a function that generates PID. This PID value will be used in the Passenger class by using getter function.
 	public Passenger signIn(Passenger p); 
 
@@ -16,7 +17,7 @@ public interface PassengerDao {
 	public List<Passenger> searchPassenger(); // Booking will be made by entering the BusId. First search function will run then the user can book the bus by entering the busId.
 	public void book(Booking bo); //generate ScheduleId within the book function to store the Id value in Schedule table.Make the Schedule ID static.
 	public List<Passenger> searchPassenger(Schedule sc);
-	public int getMaxPid();
+	public int getMaxPid();// function to get the PID
 }
 
 
